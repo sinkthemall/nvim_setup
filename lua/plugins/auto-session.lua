32c46726ed039d4a3da71c6196f9dd1dea76a5f0
d4rkn19ht <d4rk1nblade@gmail.com>
2025-10-08 06:13:46 +0700

    add marks.nvim

---
+++ lua/plugins/auto-session.lua
@@ -0,0 +1,12 @@
+return {
+    "rmagatti/auto-session",
+    lazy = false,
+    ---enables autocomplete for opts
+    opts = {
+        suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+        enabled = true,
+        auto_save = true,
+        auto_restore = true,
+    -- log_level = 'debug',
+    },
+}
